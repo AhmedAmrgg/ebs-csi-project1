075626976e9cd514b229ad07997a9a08f7157263
amora2312001 <amora2312001@gmail.com>
2025-08-10 23:30:22 +0300

    test

--- modules/EKS/versions.tf
+++ modules/EKS/versions.tf
@@ -5,16 +5,16 @@ terraform {
       version = "~> 6.0"
     }
   }
-  # Adding Backend as S3 for Remote State Storage
-  backend "s3" {
-    bucket = "terraform-on-aws-eks-amora"
-    key    = "dev/eks-cluster/terraform.tfstate"
-    region = "us-east-1" 
+#   # Adding Backend as S3 for Remote State Storage
+#   backend "s3" {
+#     bucket = "terraform-on-aws-eks-amora"
+#     key    = "dev/eks-cluster/terraform.tfstate"
+#     region = "us-east-1" 
  
-    # For State Locking
-    dynamodb_table = "dev-eks-irsa-demo"    
-  }  
-}
+#     # For State Locking
+#     dynamodb_table = "dev-eks-irsa-demo"    
+#   }  
+# }
 
 
 # Configure the AWS Provider
--- modules/ebs-terraform/versions.tf
+++ modules/ebs-terraform/versions.tf
@@ -5,20 +5,20 @@ terraform {
     aws = {
       source = "hashicorp/aws"
       #version = ">= 4.65"
-      version = ">= 5.31"
+      version = ">= 6.8.0"
      }
     helm = {
       source = "hashicorp/helm"
       #version = "2.4.1"
       #version = "~> 2.4"
-      version = "~> 2.9"
+      version = "~> 3.0.2"
     }
-    http = {
-      source = "hashicorp/http"
-      #version = "2.1.0"
-      #version = "~> 2.1"
-      version = "~> 3.3"
-    }
+    # http = {
+    #   source = "hashicorp/http"
+    #   #version = "2.1.0"
+    #   #version = "~> 2.1"
+    #   version = "~> 3.3"
+    # }
   }
   # Adding Backend as S3 for Remote State Storage
   backend "s3" {
@@ -35,7 +35,7 @@ provider "aws" {
   region = var.aws_region
 }
 
-# Terraform HTTP Provider Block
-provider "http" {
-  # Configuration options
-}
+# # Terraform HTTP Provider Block
+# provider "http" {
+#   # Configuration options
+# }
