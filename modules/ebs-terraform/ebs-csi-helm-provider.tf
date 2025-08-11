72ffed11553a2572bf244615ab248153dd81b642
amora2312001 <amora2312001@gmail.com>
2025-08-11 20:47:30 +0300

    test

--- modules/ebs-terraform/ebs-csi-helm-provider.tf
+++ modules/ebs-terraform/ebs-csi-helm-provider.tf
@@ -23,11 +23,11 @@ data "aws_eks_cluster_auth" "eks_cluster" {
 }
 
 
-provider "kubernetes" {
-  host                   = var.cluster_endpoint
-  cluster_ca_certificate = base64decode(var.cluster_ca)
-  token                  = var.cluster_token
-}
+# provider "kubernetes" {
+#   host                   = var.cluster_endpoint
+#   cluster_ca_certificate = base64decode(var.cluster_ca)
+#   token                  = var.cluster_token
+# }
 
 provider "helm" {
   kubernetes {
