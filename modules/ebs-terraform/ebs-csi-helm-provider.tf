a60d20ca97da74a485b7a6f64c3fbf48bf7652b4
amora2312001 <amora2312001@gmail.com>
2025-08-11 13:51:52 +0300

    test

--- modules/ebs-terraform/ebs-csi-helm-provider.tf
+++ modules/ebs-terraform/ebs-csi-helm-provider.tf
@@ -1,13 +1,9 @@
-# Datasource: EKS Cluster Auth 
-data "aws_eks_cluster_auth" "cluster" {
-  name = data.terraform_remote_state.eks.outputs.cluster_id
+data "aws_eks_cluster" "eks_cluster" {
+  name = module.EKS.cluster_name
 }
 
-# HELM Provider
-provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks_cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks_cluster.token
+data "aws_eks_cluster_auth" "eks_cluster" {
+  name = module.EKS.cluster_name
 }
 
 provider "helm" {
