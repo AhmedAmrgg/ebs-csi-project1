fea90271374d20d357272f974252d6c3fc684c76
amora2312001 <amora2312001@gmail.com>
2025-08-11 22:47:20 +0300

    test

--- envs/pre-prod/main.tf
+++ envs/pre-prod/main.tf
@@ -46,6 +46,6 @@ module "ebs-terraform" {
   cluster_endpoint = module.EKS.cluster_endpoint
   cluster_ca       = module.EKS.cluster_ca
   cluster_token    = data.aws_eks_cluster_auth.eks_cluster.token.
-  
-  depends_on = [module.EKS]
+  cluster_id = module.EKS.cluster_id
+  # depends_on = [module.EKS]
 }
