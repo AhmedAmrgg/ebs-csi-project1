90aa6878f141698b578c394d732f2692799353f1
amora2312001 <amora2312001@gmail.com>
2025-08-12 17:49:07 +0300

    test2

--- modules/sampleapp/versions.tf
+++ modules/sampleapp/versions.tf
@@ -1,16 +1,16 @@
-# Terraform Settings Block
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      # version = "~> 6.8.0"
-     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.7.1"
-    }     
-  }
+# # Terraform Settings Block
+# terraform {
+#   required_version = ">= 1.0.0"
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#       # version = "~> 6.8.0"
+#      }
+#     kubernetes = {
+#       source  = "hashicorp/kubernetes"
+#       version = "~> 2.7.1"
+#     }     
+#   }
   # # Adding Backend as S3 for Remote State Storage
   # backend "s3" {
   #   bucket = "terraform-on-aws-eks-amora"
