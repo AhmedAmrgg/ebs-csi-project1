01a0de226036ef817656333bb728e9fc3393ebe5
amora2312001 <amora2312001@gmail.com>
2025-08-12 17:54:12 +0300

    test2

--- envs/pre-prod/providers.tf
+++ envs/pre-prod/providers.tf
@@ -17,11 +17,11 @@ provider "aws" {
   region = var.aws_region
 }
 
-# provider "kubernetes" {
-#   host = data.terraform_remote_state.eks.outputs.cluster_endpoint 
-#   cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
-#   token = data.aws_eks_cluster_auth.cluster.token
-# }
+provider "kubernetes" {
+  host = data.aws_eks_cluster.eks_cluster.endpoint 
+  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+  token = data.aws_eks_cluster_auth.eks_cluster.token
+}
 data "aws_eks_cluster" "eks_cluster" {
   name = var.cluster_name
 }
