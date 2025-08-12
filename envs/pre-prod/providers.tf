3f8b17c3ae13e56e89e00ffddc79ce2a8c938e89
amora2312001 <amora2312001@gmail.com>
2025-08-12 17:55:10 +0300

    test2

--- envs/pre-prod/providers.tf
+++ envs/pre-prod/providers.tf
@@ -19,7 +19,7 @@ provider "aws" {
 
 provider "kubernetes" {
   host = data.aws_eks_cluster.eks_cluster.endpoint 
-  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
   token = data.aws_eks_cluster_auth.eks_cluster.token
 }
 data "aws_eks_cluster" "eks_cluster" {
