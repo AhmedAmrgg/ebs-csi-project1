85904b9e024e3da78b68e9f6412598a636eb1b30
amora2312001 <amora2312001@gmail.com>
2025-08-12 16:38:00 +0300

    test1

--- modules/ebs-terraform/ebs-csi-install-using-helm.tf
+++ modules/ebs-terraform/ebs-csi-install-using-helm.tf
@@ -1,33 +1,33 @@
-# # Install EBS CSI Driver using HELM
-# # Resource: Helm Release 
-# resource "helm_release" "ebs_csi_driver" {
-#   depends_on = [aws_iam_role.ebs_csi_iam_role]
-#   name       = "${local.name}-aws-ebs-csi-driver"
-#   repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
-#   chart      = "aws-ebs-csi-driver"
-#   namespace = "kube-system"     
+# Install EBS CSI Driver using HELM
+# Resource: Helm Release 
+resource "helm_release" "ebs_csi_driver" {
+  depends_on = [aws_iam_role.ebs_csi_iam_role]
+  name       = "${local.name}-aws-ebs-csi-driver"
+  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+  chart      = "aws-ebs-csi-driver"
+  namespace = "kube-system"     
 
-#   set {
-#     name = "image.repository"
-#     value = "602401143452.dkr.ecr.us-east-1.amazonaws.com/eks/aws-ebs-csi-driver" # Changes based on Region - This is for us-east-1 Additional Reference: https://docs.aws.amazon.com/eks/latest/userguide/add-ons-images.html
-#   }       
+  set {
+    name = "image.repository"
+    value = "602401143452.dkr.ecr.us-east-1.amazonaws.com/eks/aws-ebs-csi-driver" # Changes based on Region - This is for us-east-1 Additional Reference: https://docs.aws.amazon.com/eks/latest/userguide/add-ons-images.html
+  }       
 
-#   set {
-#     name  = "controller.serviceAccount.create"
-#     value = "true"
-#   }
+  set {
+    name  = "controller.serviceAccount.create"
+    value = "true"
+  }
 
-#   set {
-#     name  = "controller.serviceAccount.name"
-#     value = "ebs-csi-controller-sa"
-#   }
+  set {
+    name  = "controller.serviceAccount.name"
+    value = "ebs-csi-controller-sa"
+  }
 
-#   set {
-#     name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
-#     value = "${aws_iam_role.ebs_csi_iam_role.arn}"
-#   }
+  set {
+    name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = "${aws_iam_role.ebs_csi_iam_role.arn}"
+  }
     
-# }
+}
 
 # resource "helm_release" "ebs_csi_driver" {
 #   name       = "aws-ebs-csi-driver"
@@ -56,22 +56,22 @@
 # }
 
 
-resource "helm_release" "ebs_csi_driver" {
-  name       = "aws-ebs-csi-driver"
-  depends_on = [aws_iam_role.ebs_csi_iam_role]
-  repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
-  chart      = "aws-ebs-csi-driver"
-  namespace  = "kube-system"
-  version    = "2.27.0" # أو أي نسخة مناسبة
+# resource "helm_release" "ebs_csi_driver" {
+#   name       = "aws-ebs-csi-driver"
+#   depends_on = [aws_iam_role.ebs_csi_iam_role]
+#   repository = "https://kubernetes-sigs.github.io/aws-ebs-csi-driver"
+#   chart      = "aws-ebs-csi-driver"
+#   namespace  = "kube-system"
+#   version    = "2.27.0" # أو أي نسخة مناسبة
 
-  set = [
-    {
-      name  = "controller.serviceAccount.create"
-      value = "false"
-    },
-    {
-      name  = "controller.serviceAccount.name"
-      value = "ebs-csi-controller-sa"
-    }
-  ]
-}
+#   set = [
+#     {
+#       name  = "controller.serviceAccount.create"
+#       value = "false"
+#     },
+#     {
+#       name  = "controller.serviceAccount.name"
+#       value = "ebs-csi-controller-sa"
+#     }
+#   ]
+# }
