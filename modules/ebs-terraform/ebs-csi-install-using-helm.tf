6709596f63a4adf8fd5732083120510183968232
amora2312001 <amora2312001@gmail.com>
2025-08-12 16:43:53 +0300

    test1

--- modules/ebs-terraform/ebs-csi-install-using-helm.tf
+++ modules/ebs-terraform/ebs-csi-install-using-helm.tf
@@ -7,21 +7,21 @@ resource "helm_release" "ebs_csi_driver" {
   chart      = "aws-ebs-csi-driver"
   namespace = "kube-system"     
 
-  set [
+  set = [
   {
     name = "image.repository"
     value = "602401143452.dkr.ecr.us-east-1.amazonaws.com/eks/aws-ebs-csi-driver" # Changes based on Region - This is for us-east-1 Additional Reference: https://docs.aws.amazon.com/eks/latest/userguide/add-ons-images.html
-  }       
+  },     
 
   {
     name  = "controller.serviceAccount.create"
     value = "true"
-  }
+  },
 
   {
     name  = "controller.serviceAccount.name"
     value = "ebs-csi-controller-sa"
-  }
+  },
 
   {
     name  = "controller.serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
