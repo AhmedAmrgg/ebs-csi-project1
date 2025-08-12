0e116a7832ff692c6eeeff90ee244418221954d5
amora2312001 <amora2312001@gmail.com>
2025-08-12 15:50:46 +0300

    test1

--- modules/ebs-terraform/ebs-csi-helm-provider.tf
+++ modules/ebs-terraform/ebs-csi-helm-provider.tf
@@ -1,18 +1,18 @@
-data "aws_eks_cluster" "eks_cluster" {
-  name = var.cluster_name
-}
+# data "aws_eks_cluster" "eks_cluster" {
+#   name = var.cluster_name
+# }
 
-data "aws_eks_cluster_auth" "eks_cluster" {
-  name = var.cluster_name
-}
+# data "aws_eks_cluster_auth" "eks_cluster" {
+#   name = var.cluster_name
+# }
 
-provider "helm" {
-  kubernetes = {
-    host                   = data.aws_eks_cluster.eks_cluster.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.eks_cluster.token
-  }
-}
+# provider "helm" {
+#   kubernetes = {
+#     host                   = data.aws_eks_cluster.eks_cluster.endpoint
+#     cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks_cluster.certificate_authority[0].data)
+#     token                  = data.aws_eks_cluster_auth.eks_cluster.token
+#   }
+# }
 
 # data "aws_eks_cluster" "eks_cluster" {
 #   name = var.cluster_name
