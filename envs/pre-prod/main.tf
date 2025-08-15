3278af1125ef7819eac3dfb5ac2b0a862bfbeb7b
amora2312001 <amora2312001@gmail.com>
2025-08-15 17:42:49 +0300

    eks

--- envs/pre-prod/main.tf
+++ envs/pre-prod/main.tf
@@ -2,9 +2,9 @@
 module "EKS" {
   source = "../../modules/EKS"
  
-  environment = var.environment
+  # environment = var.environment
   cluster_name = var.cluster_name
-  aws_region = var.aws_region
+  # aws_region = var.aws_region
   subnet_ids   = module.Network.public_subnets # ✅ الحل هنا
   private_subnets = module.Network.private_subnets
   public_subnets  = module.Network.public_subnets
