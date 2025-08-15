c3f37d0380a5cfd4d2e76cc2b54ee5fd5115ab7d
amora2312001 <amora2312001@gmail.com>
2025-08-15 18:26:21 +0300

    eks

--- envs/pre-prod/providers.tf
+++ envs/pre-prod/providers.tf
@@ -25,13 +25,13 @@ provider "kubernetes" {
 data "aws_eks_cluster" "eks_cluster" {
   name = var.cluster_name
 }
-
+ 
 
 
 data "aws_eks_cluster_auth" "eks_cluster" {
   name = var.cluster_name
 }
-
+ 
 provider "helm" {
   kubernetes = {
     host                   = data.aws_eks_cluster.eks_cluster.endpoint
