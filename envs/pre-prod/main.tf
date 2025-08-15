a713d77854fc08f5dd19214da30058d9da2b7fbd
amora2312001 <amora2312001@gmail.com>
2025-08-15 18:01:39 +0300

    eks

--- envs/pre-prod/main.tf
+++ envs/pre-prod/main.tf
@@ -1,29 +1,29 @@
 
-module "EKS" {
-  source = "../../modules/EKS"
+# module "EKS" {
+#   source = "../../modules/EKS"
  
-  # environment = var.environment
-  cluster_name = var.cluster_name
-  # aws_region = var.aws_region
-  subnet_ids   = module.Network.public_subnets # ✅ الحل هنا
-  private_subnets = module.Network.private_subnets
-  public_subnets  = module.Network.public_subnets
+#   # environment = var.environment
+#   cluster_name = var.cluster_name
+#   # aws_region = var.aws_region
+#   subnet_ids   = module.Network.public_subnets # ✅ الحل هنا
+#   private_subnets = module.Network.private_subnets
+#   public_subnets  = module.Network.public_subnets
   
-}
+# }
  
-module "Network" {
-  source = "../../modules/Network"
+# module "Network" {
+#   source = "../../modules/Network"
 
-  vpc_name = var.vpc_name
-  vpc_cidr_block = var.vpc_cidr_block
-  vpc_availability_zones            = var.vpc_availability_zones
-  vpc_public_subnets  = var.vpc_public_subnets
-  vpc_private_subnets = var.vpc_private_subnets 
-  vpc_create_database_subnet_group      = true
-  vpc_create_database_subnet_route_table = true
-  vpc_database_subnets = var.vpc_database_subnets
+#   vpc_name = var.vpc_name
+#   vpc_cidr_block = var.vpc_cidr_block
+#   vpc_availability_zones            = var.vpc_availability_zones
+#   vpc_public_subnets  = var.vpc_public_subnets
+#   vpc_private_subnets = var.vpc_private_subnets 
+#   vpc_create_database_subnet_group      = true
+#   vpc_create_database_subnet_route_table = true
+#   vpc_database_subnets = var.vpc_database_subnets
     
-}
+# }
  
 
 
