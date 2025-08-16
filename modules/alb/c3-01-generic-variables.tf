f805bb60c56a1f268b4cfbd0eff7fb7d13b57fcf
amora2312001 <amora2312001@gmail.com>
2025-08-16 17:11:44 +0300

    project

--- modules/alb/c3-01-generic-variables.tf
+++ modules/alb/c3-01-generic-variables.tf
@@ -6,8 +6,29 @@ variable "aws_region" {
   default = "us-east-1"  
 }
 
+# modules/ebs-terraform/variables.tf
 variable "cluster_name" {
   description = "Name of the EKS cluster. Also used as a prefix in names of related resources."
   type        = string
   default     = "eksdemo"
 }
+
+variable "cluster_endpoint" {
+  type = string
+}
+variable "cluster_ca" {
+  type = string
+}
+
+variable "cluster_token" {
+  type = string
+}
+
+variable "oidc_provider_arn" {
+  type = string
+}
+
+variable "oidc_provider_url" {
+  type = string
+}
+ 
